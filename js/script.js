cfdd344533bb2555455c42635aa8c1d6fbf013d7
vinaifana <vinaifana@gmail.com>
2023-12-01 13:54:32 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// Image Slider Start
+// const slides = document.querySelectorAll(".slide")
+// var counter = 0;
+// // console.log(slides);
+
+// slides.forEach(
+//     (slide,index) => {
+//         slide.style.left = `${index * 100}%`
+//     } 
+// )
+
+
+// const goPrev = () => {
+//     counter--
+//     slideImage( )
+// }
+
+// const goNext = () => {
+//     counter++
+//     slideImage( )
+// }
+
+// const slideImage = () => {
+//     slides.forEach(
+//         (slide) => {
+//             slide.style.transform = `translateX(-${counter * 100}%)`
+//         }
+//     )
+// }
+// // Image Slider End
+
+<script src="dist/js/script.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
